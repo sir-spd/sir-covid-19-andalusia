d3285966e4d47eca51a333c9da7e2f725ff0298d
Adolfo Gastalver Rubio <agastalver@ingelectus.com>
2020-04-02 19:42:43 +0200

    first commit and updating 2020-04-02

---
+++ README.md
@@ -0,0 +1,111 @@
+# COVID-19 SIR mathematical model for Spain
+
+SIR mathematical model for infectious diseases optimized for COVID-19 using Spanish Ministry of Health public available data for Andalusia.
+
+-----
+
+![sir-cases](https://github.com/agastalver/sir-covid-19-andalusia/raw/master/images/generated-sir-cases.png "SIR Model Cases")
+
+![sir](https://github.com/agastalver/sir-covid-19-andalusia/raw/master/images/generated-sir.png "SIR Model")
+
+**Note**: 
+
+* `susceptible`, `infected`, and `recovered`, stand for the observable population; thus, the ones that can be detected. The actual values could be higher.
+* `susceptible` is an estimated value calculated from `N` with respect to the `infected` and `recovered`, the actual value is not provided by the government.
+* `recovered` stands for people who has recovered from the virus or has died; as considered in the SIR model.
+
+-----
+
+## Last optimization
+
+### Case forecasting table
+
+| Date           | Infected  | Cases      |
+|:--------------:|:---------:|:----------:|
+| 2020-04-02     | 6452      | 7048       |
+| 2020-04-03     | 6836      | 7505       |
+| 2020-04-04     | 7180      | 7928       |
+| 2020-04-05     | 7481      | 8311       |
+| 2020-04-06     | 7740      | 8655       |
+| 2020-04-07     | 7957      | 8960       |
+| 2020-04-08     | 8134      | 9227       |
+| 2020-04-09     | 8275      | 9459       |
+| 2020-04-10     | 8382      | 9659       |
+| 2020-04-11     | 8460      | 9831       |
+| 2020-04-12     | 8512      | 9978       |
+| 2020-04-13     | 8575      | 10137      |
+| 2020-04-14     | 8607      | 10265      |
+| **2020-04-15** | **8614**  | **10368**  |
+| 2020-04-16     | 8601      | 10451      |
+| 2020-04-17     | 8571      | 10517      |
+| 2020-04-18     | 8529      | 10570      |
+| 2020-04-19     | 8476      | 10612      |
+| 2020-04-20     | 8416      | 10646      |
+| 2020-04-21     | 8349      | 10673      |
+| 2020-04-22     | 8278      | 10695      |
+| 2020-04-23     | 8203      | 10712      |
+
+### Optimized SIR parameters
+
+```
+N = 10787.061909393004
+beta = 0.2730366919758111
+gamma = 0.011050376961748555
+delta = 0.7678320163629395
+delay = 0
+```
+
+* `delta` stands for a factor of reduction of `beta` during the lockdown.
+* All results are available in the folders `images` and `data`.
+
+-----
+
+## Last data information
+
+### Andalusia total cases
+
+![total](https://github.com/agastalver/sir-covid-19-andalusia/raw/master/images/generated-total.png "Total cases")
+
+-----
+
+## Methodology
+
+### Data sources
+
+Main data source:
+
+* Spanish government: https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/situacionActual.htm
+  * https://covid19.isciii.es/
+  * https://covid19.isciii.es/resources/serie_historica_acumulados.csv
+
+### Epidemiology model
+
+This is based on the SIR epidemiology model proposed by W. O. Kermack and A. G. McKendrick in:
+
+```
+Kermack, W. O.; McKendrick, A. G. (1927). "A Contribution to the Mathematical Theory of Epidemics". Proceedings of the Royal Society A: Mathematical, Physical and Engineering Sciences. 115 (772): 700. Bibcode:1927RSPSA.115..700K. doi:10.1098/rspa.1927.0118. JSTOR 94815.
+```
+
+You can find the description on wikipedia: https://en.wikipedia.org/wiki/Mathematical_modelling_of_infectious_disease#The_SIR_model
+
+### Optimization
+
+The parameters `N`, `beta`, and `gamma`, are unknown. Additionally, a `delay` parameter is considered; defined as a time synchronization parameter in case the available data is not adjusted to day 0.
+
+The optimization method is Nendel-Mead, adjusting mean squares on `I(t)` and `R(t)` to the available data. `S(t)` and `N` are calculated backwards.
+
+```
+Nelder, John A.; R. Mead (1965). "A simplex method for function minimization". Computer Journal. 7 (4): 308–313. doi:10.1093/comjnl/7.4.308.
+```
+
+## Usage
+
+Execute:
+
+```
+$ python main.py
+```
+
+## Important note
+
+The mathematical model could not adjust to the real behaviour of the virus. Furthermore, the data can have errors, delays, or be incomplete, so the adjustment can vary in the future.
---
+++ main.py
@@ -0,0 +1,257 @@
+import os
+import requests
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from scipy import optimize
+from scipy.integrate import odeint
+
+if not os.path.exists("data"):
+    os.makedirs("data")
+
+if not os.path.exists("images"):
+    os.makedirs("images")
+
+# download file
+
+url = "https://covid19.isciii.es/resources/serie_historica_acumulados.csv"
+s = requests.get(url).text
+
+#url2 = "https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/nacional_covid19.csv"
+#s2 = requests.get(url2).text
+
+# save file
+
+fn = os.path.join("data","serie_historica_acumulados.csv")
+with open(fn, "w") as f:
+    f.write(s)
+
+#fn2 = os.path.join("data","nacional_covid19.csv")
+#with open(fn2, "w") as f2:
+#    f2.write(s2)
+
+# read file
+
+df = pd.read_csv(fn, encoding="latin1")
+
+# prepare
+
+df = df[:-1]
+df = df.fillna(0)
+df.columns = ["ccaa", "date", "cases", "hospitalized", "uci", "dead", "recovered"]
+df["date"] = pd.to_datetime(df["date"], format="%d/%m/%Y")
+
+dft = df[df["ccaa"]=="AN"][["date", "cases", "hospitalized", "uci", "dead", "recovered"]]
+dft = dft[dft["cases"] > 1]
+dft = dft.set_index("date")
+dft = dft.fillna(0)
+
+dfp = df.pivot(index="date", columns="ccaa", values="cases")
+
+dfto = dft.copy()
+
+dfpct = 100*dft["dead"]/dft["cases"]
+dft["recovered"] = dft["recovered"] + dft["dead"] # as SIR model defines
+dft["infected"] = dft["cases"] - dft["recovered"]
+
+# optimization SIR
+
+inf0 = dft["infected"].values[0]
+rec0 = dft["recovered"].values[0]
+days = len(dft)
+
+def sir(N, beta, gamma, days):
+    I0 = inf0
+    R0 = rec0
+    S0 = N - I0 - R0
+
+    def deriv(y, t, N, beta, gamma):
+        S, I, R = y
+        dSdt = -beta * S * I / N
+        dIdt = beta * S * I / N - gamma * I
+        dRdt = gamma * I
+        return dSdt, dIdt, dRdt
+    
+    t = np.linspace(0, days, days)
+    y0 = S0, I0, R0
+    ret = odeint(deriv, y0, t, args=(N, beta, gamma))
+    S, I, R = ret.T
+
+    return S, I, R
+
+def sir_lockdown(N, beta, gamma, days, delta, lckday1, lckday2):
+    I0 = inf0
+    R0 = rec0
+    S0 = N - I0 - R0
+
+    def deriv(y, t, N, beta, gamma, delta):
+        S, I, R = y
+        dSdt = -beta * delta * S * I / N
+        dIdt = beta * delta * S * I / N - gamma * I
+        dRdt = gamma * I
+        return dSdt, dIdt, dRdt
+    
+    t = np.linspace(0, days, days)
+
+    y0 = S0, I0, R0
+    t0 = t[:min(lckday1+1, days)]
+    ret = odeint(deriv, y0, t0, args=(N, beta, gamma, 1))
+    S, I, R = ret.T
+
+    if lckday1 < days:
+        y1 = S[-1], I[-1], R[-1]
+        t1 = t[lckday1:min(lckday2+1, days)]
+        ret = odeint(deriv, y1, t1, args=(N, beta, gamma, delta))
+        S1, I1, R1 = ret.T
+        S, I, R = np.concatenate((S, S1[1:])), np.concatenate((I, I1[1:])), np.concatenate((R, R1[1:]))
+
+    if lckday2 < days:
+        y2 = S1[-1], I1[-1], R1[-1]
+        t2 = t[lckday2:]
+        ret = odeint(deriv, y2, t2, args=(N, beta, gamma, 1))
+        S2, I2, R2 = ret.T
+        S, I, R = np.concatenate((S, S2[1:])), np.concatenate((I, I2[1:])), np.concatenate((R, R2[1:]))
+
+    return S, I, R
+
+def fdelay(delay):
+    def f(x):
+        N = x[0]
+        beta = x[1]
+        gamma = x[2]
+        days = len(dft)
+        S, I, R = sir(N, beta, gamma, days + delay)
+        S, I, R = S[delay:delay+days], I[delay:delay+days], R[delay:delay+days]
+        Io, Ro = dft["infected"].values, dft["recovered"].values
+        #So = N - Io
+        #loss = ((S - So)**2).sum()/days + ((I - Io)**2).sum()/days + ((R - Ro)**2).sum()/days
+        loss = ((I - Io)**2).sum()/days + ((R - Ro)**2).sum()/days
+        return loss
+
+    result = optimize.minimize(f, [100000, 0.5, 0.05], method="Nelder-Mead")
+
+    return result
+
+def fdelay_lockdown(delay, lckday, nlckdays):
+    def f(x):
+        N = x[0]
+        beta = x[1]
+        gamma = x[2]
+        delta = x[3]
+        days = len(dft)
+        S, I, R = sir_lockdown(N, beta, gamma, days + delay, delta, lckday + delay, lckday + nlckdays + delay)
+        S, I, R = S[delay:delay+days], I[delay:delay+days], R[delay:delay+days]
+        Io, Ro = dft["infected"].values, dft["recovered"].values
+        #So = N - Io
+        #loss = ((S - So)**2).sum()/days + ((I - Io)**2).sum()/days + ((R - Ro)**2).sum()/days
+        loss = ((I - Io)**2).sum()/days + ((R - Ro)**2).sum()/days
+        return loss
+
+    result = optimize.minimize(f, [100000, 0.5, 0.05, 0.5], method="Nelder-Mead")
+
+    return result
+
+lckday = dft.index.get_loc(pd.to_datetime("2020-03-15"))
+nlckdays = 28 # days
+
+delay = 0 # days back
+result = fdelay_lockdown(delay, lckday, nlckdays)
+for d in range(5):
+    #res = fdelay(d)
+    res = fdelay_lockdown(d, lckday, nlckdays)
+    print("delay: {}, fun: {}".format(d, res.fun))
+    if res.fun < result.fun:
+        delay = d
+        result = res
+
+N = result.x[0]
+beta = result.x[1]
+gamma = result.x[2]
+delta = result.x[3]
+
+print("optimal: N = {}, beta = {}, gamma = {}, delta = {}, delay = {}".format(N, beta, gamma, delta, delay))
+print("error: {}".format(result.fun))
+
+#S, I, R = sir(N, beta, gamma, days + delay)
+S, I, R = sir_lockdown(N, beta, gamma, days + delay, delta, lckday + delay, lckday + nlckdays + delay)
+S, I, R = S[delay:delay+days], I[delay:delay+days], R[delay:delay+days]
+
+dft["S"] = S
+dft["I"] = I
+dft["R"] = R
+
+dft["susceptible"] = N - dft["infected"]
+
+# forecasting
+
+far = 60 # days
+
+#S, I, R =  sir(N, beta, gamma, days + far + delay)
+S, I, R = sir_lockdown(N, beta, gamma, days + far + delay, delta, lckday + delay, lckday + nlckdays + delay)
+S, I, R = S[delay:delay+days+far], I[delay:delay+days+far], R[delay:delay+days+far]
+d = {
+    "S": S,
+    "I": I,
+    "R": R,
+    "susceptible": list(dft["susceptible"].values) + [np.nan]*far,
+    "infected": list(dft["infected"].values) + [np.nan]*far,
+    "recovered": list(dft["recovered"].values) + [np.nan]*far,
+}
+dff = pd.DataFrame(d)
+dff["date"] = pd.date_range(dft.index[0],periods=days+far,freq="D")
+dff = dff.set_index("date")
+
+dff["cases"] = dff["recovered"] + dff["infected"]
+dff["forecast"] = dff["R"] + dff["I"]
+dff[["forecast", "cases"]].to_csv(os.path.join("data", "generated-cases.csv"))
+
+# graph
+
+metadata = {'Creator': None, 'Producer': None, 'CreationDate': None}
+
+fig, ax = plt.subplots(figsize=(8,6))
+dfto[["cases", "hospitalized", "uci", "dead", "recovered"]].plot(ax=ax)
+ax.set_title("Totals in Andalusia")
+ax.set_xlabel("")
+ax.set_ylabel("# of occurences")
+ax.grid(True, which="both")
+dfto[["cases", "hospitalized", "uci", "dead", "recovered"]].to_csv(os.path.join("data", "generated-total.csv"))
+plt.savefig(os.path.join("images", "generated-total.png"), format="png", dpi=300)
+plt.savefig(os.path.join("images", "generated-total.pdf"), format="pdf", dpi=300, metadata=metadata)
+
+fig, ax = plt.subplots(figsize=(8,6))
+dfpct.plot(ax=ax)
+ax.set_title("Percentage of dead")
+ax.set_xlabel("")
+ax.set_ylabel("%")
+ax.grid(True, which="both")
+plt.savefig(os.path.join("images", "generated-deadpct.png"), format="png", dpi=300)
+plt.savefig(os.path.join("images", "generated-deadpct.pdf"), format="pdf", dpi=300, metadata=metadata)
+
+fig, ax = plt.subplots(figsize=(8,6))
+dff[["susceptible"]].plot(ax=ax, alpha=0.5)
+dff[["infected", "recovered"]].plot(ax=ax)
+dff[["S", "I", "R"]].plot(ax=ax, linestyle=":")
+ax.axvspan(dff.index[lckday], dff.index[lckday+nlckdays], alpha=0.1, color='red', label="_lockdown")
+plt.text(dff.index[lckday+nlckdays-3], N*0.89, 'lockdown', color='red', alpha=0.5, rotation=90)
+ax.set_title("SIR model")
+ax.set_xlabel("")
+ax.set_ylabel("# of people")
+ax.grid(True, which="both")
+dff.to_csv(os.path.join("data", "generated-sir.csv"))
+plt.savefig(os.path.join("images", "generated-sir.png"), format="png", dpi=300)
+plt.savefig(os.path.join("images", "generated-sir.pdf"), format="pdf", dpi=300, metadata=metadata)
+
+fig, ax = plt.subplots(figsize=(8,6))
+dff[["cases"]].plot(ax=ax)
+dff[["forecast"]].plot(ax=ax, linestyle=":")
+ax.axvspan(dff.index[lckday], dff.index[lckday+nlckdays], alpha=0.1, color='red', label="_lockdown")
+plt.text(dff.index[lckday+1], N*0.89, 'lockdown', color='red', alpha=0.5, rotation=90)
+ax.set_title("Cases forecasting")
+ax.set_xlabel("")
+ax.set_ylabel("# of occurences")
+ax.grid(True, which="both")
+plt.savefig(os.path.join("images", "generated-sir-cases.png"), format="png", dpi=300)
+plt.savefig(os.path.join("images", "generated-sir-cases.pdf"), format="pdf", dpi=300, metadata=metadata)
